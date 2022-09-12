9e601ccb670b139800cc3dc5218906c2fd450b4f
monm <mounimnotakatsuki@gmail.com>
2022-09-12 18:25:51 +0100

    finished api product search

--- app/Actions/FilterProducts.php
+++ app/Actions/FilterProducts.php
@@ -10,29 +10,22 @@ class FilterProducts
     public function handle(Request $request, Builder $products_query)
     {
         $page_count = $request->query('page_count', 12);
-        $subcategory = $request->query('subcategory');
+        $subcategories = $request->query('subcategories');
         $size = $request->query('size');
         $price_range = $request->query('price_range');
         $sort = $request->query('sort');
+        $search = $request->query('search');
+        $category = $request->query('category');
 
-        $products_query
-            ->when($subcategory, function ($query, $subcategory) {
-                $query->whereRelation('subcategory', 'name', $subcategory);
-            })
 
-            ->when($size, function ($query, $size) {
-                $query->whereRelation('attributes',  function ($q) use ($size) {
-                    return $q->where('name', 'sizes')->where('value', 'like', "%$size%");
-                })->get();
-            })
+        $products_query->when($price_range, function ($query, $price_range) {
+            // dd($price_range[0]);
+            $query->where(function ($q) use ($price_range) {
+                $q->whereBetween('price', $price_range)
+                    ->orWhereBetween('sale_price', $price_range);
+            });
+        })
 
-            ->when($price_range, function ($query, $price_range) {
-                // dd($price_range[0]);
-                $query->where(function ($q) use ($price_range) {
-                    $q->whereBetween('price', $price_range)
-                        ->orWhereBetween('sale_price', $price_range);
-                });
-            })
             ->when($sort, function ($query, $sort) {
                 switch ($sort) {
                     case 'latest':
@@ -53,8 +46,56 @@ class FilterProducts
                 }
             });
 
-        $products = $products_query->paginate((int) $page_count);
 
-        return $products;
+        // find search query
+        if ($search) {
+            $products_query
+                ->when($search, function ($query, $search) use ($category, $price_range) {
+                    if ($category == (null || -1)) {
+                        return $query->where('name', 'like', "%$search%")
+                            ->when($price_range, function ($query, $price_range) {
+                                // dd($price_range[0]);
+                                $query->where(function ($q) use ($price_range) {
+                                    $q->whereBetween('price', $price_range)
+                                        ->orWhereBetween('sale_price', $price_range);
+                                });
+                            });
+                    } else {
+                        $query->whereRelation('category', function ($q) use ($category) {
+                            return $q->where('id',  $category);
+                        })->where('name', 'like', "%$search%")
+
+                            ->when($price_range, function ($query, $price_range) {
+                                // dd($price_range[0]);
+                                $query->where(function ($q) use ($price_range) {
+                                    $q->whereBetween('price', $price_range)
+                                        ->orWhereBetween('sale_price', $price_range);
+                                });
+                            });
+                    }
+                });
+
+            $products = $products_query->paginate((int) $page_count);
+
+            return $products;
+        } else {
+
+            // or apply filters
+            $products_query
+                ->when($subcategories, function ($query, $subcategories) {
+                    $query->whereRelation('subcategory', function ($q) use ($subcategories) {
+                        return $q->whereIn('name',  $subcategories);
+                    });
+                })
+
+                ->when($size, function ($query, $size) {
+                    $query->whereRelation('attributes',  function ($q) use ($size) {
+                        return $q->where('name', 'sizes')->where('value', 'like', "%$size%");
+                    });
+                });
+
+            $products = $products_query->paginate((int) $page_count);
+            return $products;
+        }
     }
 }
